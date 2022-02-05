4ba2fe038af73ad49a94a2efc287e74e22f24fbb
denis kolomoyets <kolomoetc@gmail.com>
2022-02-05 23:48:57 +0200

    step2(edited gendiff)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,6 @@ const program = new Command();
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
-
+    .version('1.0.0')
+    .option('-f, --format <type>',  'output format');
 program.parse();
