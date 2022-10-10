91802cf2238085b44c14146c6b2d9f8442bdd912
Denis <kolomoetc@gmail.com>
2022-10-10 21:51:36 +0300

    rename test variable

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const correct = readFile('expected_result.txt');
+const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const correct = readFixture('expected_result.txt');
 
 test('difference', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(correct);
